c4aefb95ac1500e4479580c6c1bbf5d859f3dbba
Gomsur <robert@eddiebengtsson.se>
2024-02-26 18:06:07 +0000

    dd

--- roberts_bike_rental/settings.py
+++ roberts_bike_rental/settings.py
@@ -109,14 +109,14 @@ WSGI_APPLICATION = 'roberts_bike_rental.wsgi.application'
 #     }
 # }
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
--- roberts_bike_rental/wsgi.py
+++ roberts_bike_rental/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roberts_bike_rental.settings')
 
 application = get_wsgi_application()
 
-app = application
+#app = application
